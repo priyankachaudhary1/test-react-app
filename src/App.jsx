22d90610bf13c3101b3203de8136323a1ded6a66
priyankachaudhary1 <priyankak@ismt.edu.np>
2023-01-20 07:51:43 +0000

    Example props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { createRoot } from "react-dom/client";
+import Pet from "./Pet";
 
 const App = () => {
   return (
     <div>
-      <h1>Start Your React Journey</h1>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="dog" breed="Havanese" />
+      <Pet name="Pepper" animal="bird" breed="Cockatiel" />
+      <Pet name="Doink" animal="cat" breed="Mix" />
     </div>
   );
 };
